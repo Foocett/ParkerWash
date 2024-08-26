16ab90afb676d840f70df97c43150b8a47c26d45
Andrew Fawcett <ajfawcett@gmail.com>
2024-08-26 16:21:34 -0500

    Added visual feedback to status change

--- Public/index.js
+++ Public/index.js
@@ -1,5 +1,5 @@
 const animationInterval = 50; //time between each element fade in milliseconds
-
+let currentMachine = 0;
 const fadeSpeed = 0.15; //fade time in seconds
 
 let currentWindow = "Washer";
@@ -11,26 +11,49 @@ document.addEventListener("DOMContentLoaded", () => {
   const washerButton = document.getElementById("washer-button");
   const dryerButton = document.getElementById("dryer-button");
   const buttons = document.querySelectorAll(".list-item");
+  const allMachines = document.querySelectorAll(".grid-item");
+  const setStateBody = document.getElementById("set-state-body");
+  const allStatusTitles = document.querySelectorAll(".status");
+
+  const isSetStateWindowOpen = function () {
+    return setStateBody.style.display === "block";
+  };
 
   document.documentElement.style.setProperty("--fade-speed", `${fadeSpeed}s`);
   showWasherWindow();
+  function toggleSetStateWindow() {
+    if (setStateBody.style.display === "block") {
+      setStateBody.style.opacity = 0;
+      setTimeout(() => {
+        setStateBody.style.display = "none";
+      }, 250);
+    } else {
+      setStateBody.style.display = "block";
+      setTimeout(() => {
+        setStateBody.style.transition = "opacity 0.5s";
+        setStateBody.style.opacity = 1;
+      }, 10); // Small delay to ensure the display change is applied before the transition
+    }
+  }
 
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
-      buttons.forEach((btn) => btn.classList.remove("selected"));
-      this.classList.add("selected");
+      if (!isSetStateWindowOpen()) {
+        buttons.forEach((btn) => btn.classList.remove("selected"));
+        this.classList.add("selected");
+      }
     });
   });
 
   washerButton.addEventListener("click", () => {
-    if (currentWindow === "Dryer") {
+    if (currentWindow === "Dryer" && !isSetStateWindowOpen()) {
       showWasherWindow();
       currentWindow = "Washer";
     }
   });
 
   dryerButton.addEventListener("click", () => {
-    if (currentWindow === "Washer") {
+    if (currentWindow === "Washer" && !isSetStateWindowOpen()) {
       showDryerWindow();
       currentWindow = "Dryer";
     }
@@ -96,4 +119,54 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
+
+  for (let i = 0; i < allMachines.length; i++) {
+    allMachines[i].addEventListener("click", () => {
+      currentMachine = i;
+      if (setStateBody != "block") {
+        toggleSetStateWindow();
+      }
+    });
+  }
+
+  document.getElementById("set-state-close").addEventListener("click", () => {
+    toggleSetStateWindow();
+  });
+
+  document
+    .getElementById("set-state-available")
+    .addEventListener("click", () => {
+      toggleSetStateWindow();
+      allMachines[currentMachine].classList.remove("in-use");
+      allMachines[currentMachine].classList.remove("broken");
+      allMachines[currentMachine].classList.remove("finished");
+      allMachines[currentMachine].classList.add("available");
+      allStatusTitles[currentMachine].innerText = "Available";
+    });
+  document.getElementById("set-state-in-use").addEventListener("click", () => {
+    toggleSetStateWindow();
+    allMachines[currentMachine].classList.remove("available");
+    allMachines[currentMachine].classList.remove("broken");
+    allMachines[currentMachine].classList.remove("finished");
+    allMachines[currentMachine].classList.add("in-use");
+    allStatusTitles[currentMachine].innerText = "In Use";
+  });
+  document.getElementById("set-state-broken").addEventListener("click", () => {
+    toggleSetStateWindow();
+    allMachines[currentMachine].classList.remove("available");
+    allMachines[currentMachine].classList.remove("in-use");
+    allMachines[currentMachine].classList.remove("finished");
+    allMachines[currentMachine].classList.add("broken");
+    allStatusTitles[currentMachine].innerText = "Broken";
+  });
+  document
+    .getElementById("set-state-finished")
+    .addEventListener("click", () => {
+      toggleSetStateWindow();
+      allMachines[currentMachine].classList.remove("available");
+      allMachines[currentMachine].classList.remove("in-use");
+      allMachines[currentMachine].classList.remove("broken");
+      allMachines[currentMachine].classList.add("finished");
+      allStatusTitles[currentMachine].innerText = "Finished";
+    });
 });
